63d5653d72ff27630fc35f42b40f06c411f6f1e6
Trica Ryckman <tryckman@moresteam.com>
2016-11-08 11:48:03 -0500

    updated style

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function initMap() {
   	var map = new google.maps.Map(document.getElementById('map'), {
     	center: {lat: 40.8054491, lng: -73.9654415},
-    	zoom: 8
+    	zoom: 15,
   	});
   	var marker = new google.maps.Marker({
 		position: {lat: 40.8054491, lng: -73.9654415},
